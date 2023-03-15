266608f175a54257d6f03ed931f22658bacd0b10
Bilegt Davaa <96280882+3shadesmarimo@users.noreply.github.com>
2023-03-15 15:36:01 -0400

    Initial Commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Bilegt Davaa on 2023-03-15.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
