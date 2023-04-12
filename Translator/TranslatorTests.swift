7083e597a7ae08a4fba3f76050b7bbc62e54914a
Bilegt Davaa <96280882+3shadesmarimo@users.noreply.github.com>
2023-04-11 21:26:29 -0400

    hehe

---
+++ Translator/TranslatorTests.swift
@@ -0,0 +1,30 @@
+//
+//  TranslatorTests.swift
+//  Translator
+//
+//  Created by Bilegt Davaa on 2023-04-10.
+//
+
+/*
+import XCTest
+@testable import Translator
+
+class TranslatorTests: XCTestCase {
+    
+    func testTranslation() {
+        let translator = Translator(key: "714c72d008d74750aecaafdda23d58ee", endpoint: "https://api.cognitive.microsofttranslator.com", location: "westus2")
+        
+        let expectation = XCTestExpectation(description: "Translation expectation")
+        translator.translate(text: "Hello, how are you?", from: "en", to: ["fr"]) { result, error in
+            XCTAssertNotNil(result)
+            XCTAssertNil(error)
+            XCTAssertEqual(result, "Bonjour, comment allez-vous?")
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+}
+
+*/
